9cf914bb09c897cb75b35ebda604577c4b1b54f8
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-04-25 11:29:09 -0400

    Added the scripts used to produce extdata
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ inst/scripts/invalid_format.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+touch inst/extdata/invalid_format.bdg
---
+++ inst/scripts/valid_peaks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/CharlesJB/GenomicFormatExamples/master/inst/extdata/demo.bed -O inst/extdata/valid.bed
+wget https://raw.githubusercontent.com/CharlesJB/GenomicFormatExamples/master/inst/extdata/demo.narrowPeak -O inst/extdata/valid.narrowPeak
+wget https://raw.githubusercontent.com/CharlesJB/GenomicFormatExamples/master/inst/extdata/demo.broadPeak -O inst/extdata/valid.broadPeak
