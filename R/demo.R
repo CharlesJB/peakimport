d9a978b02af8e14f1a30e1730e0d550bb1cfa774
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-04-25 11:47:44 -0400

    Added a function to extract the demo peak files
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ R/demo.R
@@ -0,0 +1,35 @@
+#' Get the file path to a specific file format demo file.
+#'
+#' Available format:
+#'     * bed
+#'     * narrowPeak
+#'     * broadPeak
+#'
+#' @param format The file format to return. Default: "bed"
+#'
+#' @return The path to the file corresponding to the specified file format.
+#'
+#' @examples
+#' get_demo_file() # Return path to bed file by default
+#' get_demo_file(format = "narrowPeak")
+#'
+#' @export
+get_demo_file <- function(format = c("bed", "narrowPeak", "broadPeak")) {
+    format <- match.arg(format)
+    if (format == "bed") {
+        system.file("extdata",
+                    "valid.bed",
+                    package = "peakanno",
+                    mustWork = TRUE)
+    } else if (format == "narrowPeak") {
+        system.file("extdata",
+                    "valid.narrowPeak",
+                    package = "peakanno",
+                    mustWork = TRUE)
+    } else if (format == "broadPeak") {
+        system.file("extdata",
+                    "valid.broadPeak",
+                    package = "peakanno",
+                    mustWork = TRUE)
+    }
+}
